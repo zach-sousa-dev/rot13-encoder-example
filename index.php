59d7aa52e3fa57ee2515dd722c4a866b965b2860
Zachary Sousa <zs20np@snpsteam.com>
2024-02-27 14:15:27 -0500

    working encoder

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
         <meta charset="UTF-8">
 
         <title>ROT13 Encoder</title>
-        <meta name="description" content="This program shows the user 3 slots with a spin button.
-        A different message is displayed upon getting no matches, 2 matches, or a jackpot."/>
+        <meta name="description" content="This program reads a .txt file and does ROT13 encoding to each line."/>
         <meta name="author" content="Zachary Sousa"/>
 
         <link href="style.css" rel="stylesheet" type="text/css"/>
@@ -14,42 +13,84 @@
 	</head>
 
     <body> 
+
+        <h2>WELCOME TO MY REALLY COOL ROT13 ENCODER. CHANGE input.txt IN THE FILES TO GET DIFFERENT RESULTS.</h2>
+
+        <?php
+            $input_file = "input.txt";
+            $output_file = "output.txt";
+
+            $input = file_get_contents($input_file);
+            $input = explode("\n", $input);
+
+            $output = [];
+            $output_str = "";
+
+            for($i = 0; $i < count($input); $i++) {
+                echo "<strong>INPUTTED: </strong>".$input[$i]."<br>";
+                array_push($output, rot13($input[$i]));
+                $output_str.=$output[$i];
+                echo "<strong>OUTPUTTED: </strong>".$output[$i]."<br>";
+                echo "<strong>ROT26: </strong>".rot13($output[$i])."<br><br>";
+            }
+
+            file_put_contents($output_file, $output_str);
+            echo "<strong>DATA WRITTEN.</strong>";
+        ?>
+
+
         
         <?php
-            $alpha_str  = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
-            $temp_lower  = explode(" ", $alpha_str);
+            # contains rot13 function
 
-            for($i = 0; $i < count($temp_lower); $i++) {
-                if($i < 13) {
-                    $cypher_lower[$temp_lower[$i]] = $temp_lower[$i + 13]."<br>";
-                } else {
-                    $cypher_lower[$temp_lower[$i]] = $temp_lower[$i - 13]." <br>";
+            function rot13($input_str) {
+                //INIT
+                # build lowercase cypher
+                $alpha_str  = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+                $temp = explode(" ", $alpha_str);
+
+                $cypher_lower = [];
+                $cypher_upper = [];
+
+                for($i = 0; $i < count($temp); $i++) {
+                    if($i < 13) {
+                        $cypher_lower[$temp[$i]] = $temp[$i + 13];
+                    } else {
+                        $cypher_lower[$temp[$i]] = $temp[$i - 13];
+                    }
                 }
-            }
 
-            echo var_dump($temp_lower);
+                # build upperccase cypher
+                $alpha_str = strtoupper($alpha_str);
+                $temp = explode(" ", $alpha_str);
 
-            echo "<br><br>";
-
-            echo var_dump($cypher_lower);
-
-            echo "<br><br>";
-
-
-
-            $input_str = "Hello World";
-            $input_str = strtolower($input_str);
-            echo $input_str."<br>";
-
-            for($i = 0; $i < strlen($input_str); $i++) {
-                $is_ok = true;
-                for($j = 0; $j < count(explode(" ", $alpha_str); $i++) {
-                    strcmp();
+                for($i = 0; $i < count($temp); $i++) {
+                    if($i < 13) {
+                        $cypher_upper[$temp[$i]] = $temp[$i + 13];
+                    } else {
+                        $cypher_upper[$temp[$i]] = $temp[$i - 13];
+                    }
                 }
+                //END INIT
+
+
+                //CONVERSION
+                $new_str = "";
+
+                for($i = 0; $i < strlen($input_str); $i++) {
+                    if(in_array($input_str[$i], $cypher_lower)) {
+                        $new_str = $new_str.$cypher_lower[$input_str[$i]];
+                    } else {
+                        if(in_array($input_str[$i], $cypher_upper)) {
+                            $new_str = $new_str.$cypher_upper[$input_str[$i]];
+                        } else {
+                            $new_str = $new_str.$input_str[$i];
+                        }
+                    }
+                }
+                return $new_str;
+                //DONE CONVERSION
             }
-
-
-
         ?>
         
     </body>
